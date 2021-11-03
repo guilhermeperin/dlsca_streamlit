57161c2f0fc9b5893546214c5b280730f10b3c74
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-11-03 10:38:59 +0100

    updates

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title('State-of-the-art Deep Learning Models')
+
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
