a3e09cee9c7629e67d5bc2ab33ef63e6b28a4b40
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-11-03 10:50:58 +0100

    updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title('State-of-the-art Deep Learning Models')
 
-st.text('This page provides state-of-the-art deep learning models and results for open-source SCA datasets.')
+st.text_area('This page provides state-of-the-art deep learning models and results for open-source SCA datasets.')
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
