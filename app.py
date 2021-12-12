fc13b0eb0af4764987fe1b91fbf0f66bbcdbcb31
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-12-12 22:17:29 +0100

    updates

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('State-of-the-art Deep Learning Models')
+st.title('State-of-the-art Deep Learning Models for Profiling Side-Channel Analyis')
 
+st.markdown('Side channel analysis (SCA) are non-invasive attacks that explore unintended information leakage from security-aware '
+            'devices.')
 st.markdown('This page provides state-of-the-art deep learning models and results for open-source SCA datasets.')
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
+
