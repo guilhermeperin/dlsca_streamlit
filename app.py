31161086628d8134b30bda3c1db8c03ecc1023bb
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-12-14 11:21:55 +0100

    updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('Profiling Side-Channel Analyis with Deep Learning')
 
 st.markdown('Side channel analysis (SCA) are non-invasive attacks that explore unintended information leakage from electronic '
-            'devices. Electronic devices leak unintended information such as power consumption, electromagnetic emissions, time,'
+            'devices. Electronic devices leak unintended information such as power consumption, electromagnetic emissions, time, '
             'acoustic, temperature and photonic emission.')
 st.markdown('Cryptographic algorithms are mathematically secure. However, when deployed in microcontrolers, microprocessors, FPGAs or ASIC,'
             'just to mention a few, they become vulnerable to side-channel attacks due the unintended leakages. Countermeasures are usually'
@@ -13,6 +13,7 @@ st.markdown('Cryptographic algorithms are mathematically secure. However, when d
             'vulnerable to a very strong class of side-channel attacks known as profiling SCA. The process follows exactly the supervised'
             'learning procedure and we show how deep neural network can be used as the learning algorithm.')
 st.header('Profiling SCA')
+st.subheader('Preparing the dataset')
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
