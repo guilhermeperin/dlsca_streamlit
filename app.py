e273b5e92ecce7ad01c473bf0c0dc9eef5f8ee20
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-12-19 13:41:11 +0100

    updates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request as url_request
+import h5py
+
+import aisy_sca
 
 st.title('Profiling Side-Channel Analyis with Deep Learning')
 
@@ -15,6 +19,28 @@ st.markdown('Cryptographic algorithms are mathematically secure. However, when d
 st.header('Profiling SCA')
 st.subheader('Preparing the dataset')
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
+from pathlib import Path
 
+my_file = Path('ascad-variable.h5')
+if not my_file.is_file():
+    url = 'https://static.data.gouv.fr/resources/ascad-atmega-8515-variable-key/20190903-083349/ascad-variable.h5'
+    url_request.urlretrieve(url, 'ascad-variable.h5')
+
+aisy = aisy_sca.Aisy()
+aisy.set_resources_root_folder("resources/")
+aisy.set_database_root_folder("resources/databases/")
+aisy.set_datasets_root_folder("")
+aisy.set_database_name("database_ascad.sqlite")
+
+dataset_dict = {
+    "ascad-variable.h5": {
+        "filename": "ascad-variable.h5",
+        "key": "00112233445566778899AABBCCDDEEFF",
+        "first_sample": 0,
+        "number_of_samples": 1400,
+        "number_of_profiling_traces": 200000,
+        "number_of_attack_traces": 1000
+    }
+}
+
+aisy.set_dataset(dataset_dict["ascad-variable.h5"])
